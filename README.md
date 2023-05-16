844e1c92927fe96d8275e4fefaa5d598d35a60f1
Andrew Swindlehurst <andrewswindlehurst@Andrews-MacBook-Pro.local>
2023-05-16 16:10:31 +0100

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# audit-history-crawler
-A lightweight and fast method of pulling and reading Lunio account audit history.
+# Audit History Parser
+A lightweight and fast<sup>kind of</sup> method of pulling and reading Lunio account audit history.
+
+**Table of Contents**
+
+
+###Requirements
+1. Python (the script was written with version *3.9.16* but should be able to be run on higher versions).
+2. Production AWS access with permissions to read from the `poc-audit-history` bucket (read only minimum).
+
+###Usage
+First we need to install the required packages (this only needs to be done once). This can be done with the command:
+```shell
+pip install -r requirements.txt
+```
+
+Once that installation completes, we need to navigate to the [lunio aws landing page](https://lunio.awsapps.com/start#/ "lunio aws landing page") and get our session credentials  by clicking on _Command line or programmatic access_ and following the instructions on screen. These credential normally only last for a short amount of time so this set may need to be repeated.
+
+Once the setup is complete, we should be able to run the file with the command:
+```shell
+python main.py --account_id=<account_id> 
+```
+If you are looking for a specific customer or campaign, include the paramater `--aw_id=<customer/campaign_id>` and that will give you the history specific to that customer or campaign.
---
+++ main.py
@@ -0,0 +1,117 @@
+
+import argparse
+import boto3
+import os
+import shutil
+import gzip
+import pandas as pd
+import io
+import json
+from datetime import datetime
+
+
+def aw_id_target(args):
+	bucket_name = "poc-audit-history-records"
+	folder_path = "tmp"
+	prefix = f"aw_id/account_id={args.account_id}/adwords_id={args.aw_id}"
+
+	success, client, objects = bucket_rip(prefix, bucket_name, folder_path)
+	if not success:
+		return False
+
+	i = 0
+	for item in objects['Contents']:
+		i += 1
+		local_file_path = f"{folder_path}/{item['Key'].split('/')[-1]}"
+		client.download_file(bucket_name, item['Key'], local_file_path)
+		print(f"Downloaded {i} of {len(objects['Contents'])}.", end="\r")
+
+	print("Download finished. Starting parsing data.")
+
+	df = process_gz_files(folder_path)
+	print(df)
+
+	if os.path.exists(folder_path):
+		shutil.rmtree(folder_path)
+
+	output_file_name = f"{args.account_id}-{args.aw_id}-{datetime.now()}.csv"
+	df.to_csv(output_file_name, index=False)
+
+	return True
+
+def account_id_target(args):
+	print("This ins't comeplete")
+	return True
+
+def bucket_rip(prefix, bucket_name, folder_path):
+	try:
+		# Make sure user has boto3, otherwise provide a user friendly error message.
+		client = boto3.client("s3")
+	except Exception as e:
+		print(e)
+		print("Error instantiating boto3 client. Please run 'pip install boto3' before re-running this script.")
+		return False, None, None
+
+	try:
+		objects = client.list_objects_v2(
+			Bucket=bucket_name,
+			Prefix=prefix
+		)
+	except Exception as e:
+		print(e)
+		print("Error connecting to aws. Please make sure your credentials are up to date.")
+		return False, None, None
+
+	try:
+		if os.path.exists(folder_path):
+			shutil.rmtree(folder_path)
+		os.makedirs(folder_path)
+	except Exception as e:
+		print(e)
+		print("Failed to create temp folder.")
+		return False, None, None
+
+	return True, client, objects
+
+
+def process_gz_files(path):
+	file_list = os.listdir(path)
+	data_list = []
+
+	for file_name in file_list:
+	    file_path = os.path.join(path, file_name)
+
+	    if file_name.endswith('.gz'):
+	        with gzip.open(file_path, 'rt') as file:
+	            file_contents = file.read()
+	            file_contents = file_contents.split('\n')
+	            for i in range(len(file_contents)):
+	            	file_contents[i] = json.loads(file_contents[i])
+
+	        df = pd.DataFrame(file_contents)
+        	data_list.append(df)
+
+	combined_df = pd.concat(data_list, ignore_index=True)
+	return combined_df
+
+if __name__ == "__main__":
+	parser = argparse.ArgumentParser(description="Jobs main code")
+	parser.add_argument(
+		"--account_id",
+		required=True,
+		help="Account ID value of the account being searched"
+	)
+
+	parser.add_argument(
+		"--aw_id",
+		help="Customer or campaign ID"
+	)
+
+	args = parser.parse_args()
+	if args.aw_id is not None:
+		success = aw_id_target(args)
+	else:
+		success = account_id_target(args)
+	
+	message = "End" if success else "An error occurred and the script failed to complete"
+	print(f"--- {message} ---")
